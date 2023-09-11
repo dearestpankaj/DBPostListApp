0f3b1a86adcf81b12b862f3c66d873fb4e1c938e
Pankaj Sachdeva <dearestpankaj@gmail.com>
2023-09-11 22:36:29 +0200

    Restructure project supporting module based classification

---
+++ DBPostListApp/Application/SceneDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  SceneDelegate.swift
+//  DBPostListApp
+//
+//  Created by Pankaj Sachdeva on 10/09/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        let router = LoginRouter()
+        let navigationController = UINavigationController(rootViewController: router.createModule())
+        window.rootViewController = navigationController
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) { }
+
+    func sceneDidBecomeActive(_ scene: UIScene) { }
+
+    func sceneWillResignActive(_ scene: UIScene) { }
+
+    func sceneWillEnterForeground(_ scene: UIScene) { }
+
+    func sceneDidEnterBackground(_ scene: UIScene) { }
+
+
+}
+
